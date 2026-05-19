bc6f737b04092465206b16db4e40968fb5f1a2ad
hm4c8wpz7f-ai <hm4c8wpz7f@privaterelay.appleid.com>
2026-05-19 14:04:20 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,23 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
 import os
 
 TOKEN = os.environ.get("BOT_TOKEN")
+
+# /start komandasi
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "Ассалому алайкум!\nСамМой маҳсулотлари боти ишлади ✅"
+    )
+
+def main():
+    app = Application.builder().token(TOKEN).build()
+
+    # Handler
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot ishga tushdi...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
